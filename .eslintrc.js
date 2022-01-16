dc268008ac3e5245c380772c90e1d021d966a7de
Andrea Scardino <andreascardino97@gmail.com>
2022-01-16 16:03:31 +0100

    feat: initial config (eslint, react, components, constants, pages)

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': ['error', 'windows'],
+    semi: ['error', 'never'],
+    'no-restricted-exports': ['error', {
+      restrictedNamedExports: [],
+    }],
+  },
+}
